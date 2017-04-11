545921a52e9acb0b02c9a70ac052c3b9db3c4b20
LouisChen <LouisChen1992@hotmail.com>
2017-04-10 22:31:20 -0700

    firstDataCrawler

---
+++ src/utils_database.py
@@ -0,0 +1,38 @@
+import pymysql
+
+def connect_database():
+	db_info = {'host':'localhost','user':'root','passwd':'cly20070314','port':3306,'db':'sys'}
+	db = pymysql.connect(**db_info)
+	cursor = db.cursor()
+	return db, cursor
+
+def create_image_table(db, cursor):
+	cursor.execute("""
+		CREATE TABLE sImg (
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		imgURL VARCHAR(255),
+		tag VARCHAR(255),
+		timeStamp TIMESTAMP(6)
+		)""")
+
+def insert_sImg(db, cursor, urls, tag):
+	for url in urls:
+		cursor.execute("""
+			INSERT INTO sImg (imgURL, tag)
+			VALUES ('%s', '%s')
+			""" %(url, tag))
+	db.commit()
+
+def initialize_table(db, cursor):
+	cursor.execute("""
+		CREATE TABLE product (
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		imgURL VARCHAR(255),
+		brand VARCHAR(255),
+		name VARCHAR(255),
+		tag VARCHAR(255),
+		timeStamp TIMESTAMP(6)
+		)""")
+
+if __name__ == '__main__':
+	create_image_table()

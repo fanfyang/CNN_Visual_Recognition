943a1f7690abfc78748e4ad6eea397cd6259fe27
fannyfanyang <yangf.fanny@gmail.com>
2017-05-15 17:23:39 -0700

    add alexnet test

--- src/alexNet_test.py
+++ src/alexNet_test.py
@@ -1,7 +1,7 @@
 from model_alexNet import *
 
 config = Config(num_classes = 20, batch_size = 70, lr = 0.001, l2 = 0.0)
-vgg = model_vgg16_20(config)
+vgg = model_alexNet(config)
 
 x,y,z = fetch_data(file = True)
 X_train = x[:700]
@@ -11,5 +11,5 @@ y_val = y[700:]
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-vgg.load_parameters(sess,'../data/vgg16/vgg16_weights.npz',rand_init = ['fc8_W', 'fc8_b'])
+# vgg.load_parameters(sess,'../data/vgg16/vgg16_weights.npz',rand_init = ['fc8_W', 'fc8_b'])
 vgg.train(sess,X_train,y_train,X_val,y_val)

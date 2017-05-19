d1b0c360a0c67abe8ec0c9d7c4237cea1696b7e6
LouisChen <LouisChen1992@hotmail.com>
2017-05-18 23:47:20 -0700

    test

--- src/test.py
+++ src/test.py
@@ -15,13 +15,13 @@ model = args.m
 num_classes = int(args.nc)
 batch_size = int(args.bs)
 
-x,y,z = fetch_data(file = True, cate_file = 'categories_10000.txt', image_file = 'images_10000.txt')
-N = len(y)
-N_val = N // 10 * 9
-X_test = x[N_val:]
-y_test = y[N_val:]
-
 if model == 'vgg': 
+	x,y,z = fetch_data(file = True, cate_file = 'categories_10000.txt', image_file = 'images_10000.txt')
+	N = len(y)
+	N_val = N // 10 * 9
+	X_test = x[N_val:]
+	y_test = y[N_val:]
+
 	config = Config(num_classes = num_classes, batch_size = batch_size)
 	vgg = model_vgg16_20(config)
 	sess = tf.Session()
@@ -29,12 +29,23 @@ if model == 'vgg':
 	vgg.load_parameters(sess,'../model/vgg/para_' + version + '.npz')
 	print('test accu: %0.4f' % (1-vgg.error(sess,X_test-vgg._channel_mean,y_test)))
 elif model == 'svm':
+	x,y,z = fetch_data(file = True, cate_file = 'categories_10000.txt', image_file = 'images_10000.txt')
+	N = len(y)
+	N_val = N // 10 * 9
+	X_test = x[N_val:]
+	y_test = y[N_val:]
+	
 	config = Config(num_classes = num_classes, batch_size = batch_size)
 	svm = svm(config)
 	sess = tf.Session()
 	svm.load_parameters(sess,'../model/svm/para_' + version + '.npz')
 	print('test accu: %0.4f' % (1-svm.error(sess,X_test-svm._channel_mean,y_test)))
 else:
+	x,y,z = fetch_data(file = True, resize = (227,227,3), cate_file = 'categories_10000.txt', image_file = 'images_10000.txt')
+	N = len(y)
+	N_val = N // 10 * 9
+	X_test = x[N_val:]
+	y_test = y[N_val:]
 	config = Config(num_classes = num_classes, batch_size = batch_size)
 	alex = model_alexNet(config)
 	sess = tf.Session()

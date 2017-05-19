fa954b601b521db70522030b936da94e5129fed8
LouisChen <LouisChen1992@hotmail.com>
2017-05-18 20:19:00 -0700

    test

--- src/test.py
+++ src/test.py
@@ -6,10 +6,14 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--v', help = 'version')
 parser.add_argument('--m', help = 'model')
+parser.add_argument('--nc', help = 'number of classes')
+parser.add_argument('--bs', help = 'batch size')
 args = parser.parse_args()
 
 version = args.v
 model = args.m
+num_classes = int(args.nc)
+batch_size = int(args.bs)
 
 x,y,z = fetch_data(file = True, cate_file = 'categories_10000.txt', image_file = 'images_10000.txt')
 N = len(y)
@@ -18,19 +22,19 @@ X_test = x[N_val:]
 y_test = y[N_val:]
 
 if model == 'vgg': 
-	config = Config()
+	config = Config(num_classes = num_classes, batch_size = batch_size)
 	vgg = model_vgg16_20(config)
 	sess = tf.Session()
 	vgg.load_parameters(sess,'../model/vgg/para_' + version + '.npz')
 	print('test accu: %0.4f' % (1-vgg.error(sess,X_test-vgg._channel_mean,y_test)))
 elif model == 'svm':
-	config = Config()
+	config = Config(num_classes = num_classes, batch_size = batch_size)
 	svm = svm(config)
 	sess = tf.Session()
 	svm.load_parameters(sess,'../model/svm/para_' + version + '.npz')
 	print('test accu: %0.4f' % (1-svm.error(sess,X_test-vgg._channel_mean,y_test)))
 else:
-	config = Config()
+	config = Config(num_classes = num_classes, batch_size = batch_size)
 	alex = model_alexNet(config)
 	sess = tf.Session()
 	alex.load_parameters(sess,'../model/alex/para_' + version + '.npz')

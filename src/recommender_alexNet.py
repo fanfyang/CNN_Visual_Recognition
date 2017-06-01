99a7a8e7df951a7e46ac8f6a11f2c1cdfb679582
fannyfanyang <yangf.fanny@gmail.com>
2017-05-31 17:07:36 -0700

    top similarity

--- src/recommender_alexNet.py
+++ src/recommender_alexNet.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
 	features_test = run_epoch(alex, sess, x_test, y_test, shuffle = False)
 
 	print(features_base.shape, y_base.shape, features_test.shape, y_test.shape)
-	similarities = top_similar_item(features_base, y_base, features_test, y_test, 2)
+	similarities = top_similar_item(features_base, z_base, features_test, z_test, 2)
 	print(similarities)
 
 

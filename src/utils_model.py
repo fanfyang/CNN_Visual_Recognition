2f2d9e3ed78c8bf43ce9d5ef9ee920b4763e410a
LouisChen <LouisChen1992@hotmail.com>
2017-06-03 12:47:09 -0700

    util model

--- src/utils_model.py
+++ src/utils_model.py
@@ -300,3 +300,4 @@ class model(object):
 		pred = sess.run(self._pred, feed_dict)
 		label = y[num_batches*self._config.batch_size:]
 		num_correct += np.sum(pred == label)
+		return 1 - num_correct * 1.0 / X.shape[0]

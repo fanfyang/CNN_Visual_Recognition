502198e3a817efc72b7a9e2e7fcabff9df7a3408
LouisChen <LouisChen1992@hotmail.com>
2017-06-08 20:55:46 -0700

    train

--- src/train.py
+++ src/train.py
@@ -17,26 +17,23 @@ version = args.v
 
 para = parse_argument(args)
 
-config = Config(**para)
-# config = Config(num_classes = 20, batch_size = 70, lr = 0.001, l2 = 0.0)
-vgg = model_vgg16_20(config)
-
 # Example 1
-x,y,class_names = fetch_data(file = True, cate_file = 'categories_10000.txt', image_file = 'images_10000.txt')
 # x,y,class_names = fetch_data(file = True, cate_file = 'categories.txt', image_file = 'images.txt')
-x -= vgg._channel_mean
+x,y,class_names = fetch_data(file = True, cate_file = 'categories_10000.txt', image_file = 'images_10000.txt')
 N = len(y)
 N_train = N // 10 * 7
 N_val = N // 10 * 9
 X_train = x[:N_train]
-X_val = x[N_train:N_val]
 y_train = y[:N_train]
+X_val = x[N_train:N_val]
 y_val = y[N_train:N_val]
 
+config = Config(**para)
+vgg = model_vgg16_20(config)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 vgg.load_parameters(sess,'../data/vgg16/vgg16_weights.npz',rand_init = ['fc8_W', 'fc8_b'])
-vgg.train(sess,X_train,y_train,X_val,y_val,version, model = 'vgg')
+vgg.train(sess,X_train - vgg._channel_mean,y_train,X_val - vgg._channel_mean,y_val,version, model = 'vgg')
 
 # # Example 2
 # x,y,z = fetch_data_2(cate_file = 'categories.txt', image_file = 'images.txt')

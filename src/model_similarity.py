14ccff642f10aa402ec23fda4db0243c23442c15
LouisChen <LouisChen1992@hotmail.com>
2017-06-09 13:19:48 -0700

    model similarity

---
+++ src/model_similarity.py
@@ -0,0 +1,114 @@
+from utils_model import *
+import tensorflow as tf
+
+class Config_NN:
+	def __init__(self, lr = 0.001, decay_rate = 0.9, decay_steps = 700, l2 = 0.0005, input_dim = 4096, batch_size = 128, num_epoch = 20, dropout = 0.5):
+		self.lr = lr
+		self.decay_rate = decay_rate
+		self.decay_steps = decay_steps
+		self.l2 = l2
+		self.input_dim = input_dim
+		self.batch_size = batch_size
+		self.num_epoch = num_epoch
+		self.dropout = dropout
+
+class model_nn(model):
+	def __init__(self, config):
+		self._config = config
+		self._input_placeholder = tf.placeholder(dtype = tf.float32, shape = (None, self._config.input_dim))
+		self._output_placeholder = tf.placeholder(dtype = tf.float32, shape = (None,))
+		self._parameters = dict()
+
+		with tf.variable_scope('nn/fc1') as scope:
+			Wfc1 = tf.get_variable('W',[self._config.input_dim,512], trainable = True, initializer = tf.contrib.layers.xavier_initializer())
+			bfc1 = tf.get_variable('b',[512], trainable = True, initializer = tf.contrib.layers.xavier_initializer())
+			fc1 = tf.nn.relu(tf.matmul(self._input_placeholder, Wfc1) + bfc1, name = 'fc1')
+			self._parameters['fc1_W'] = Wfc1
+			self._parameters['fc1_b'] = bfc1
+			tf.add_to_collection('Reg', tf.reduce_sum(tf.square(Wfc1)))
+
+		with tf.variable_scope('nn/fc2') as scope:
+			Wfc2 = tf.get_variable('W',[512,32], trainable = True, initializer = tf.contrib.layers.xavier_initializer())
+			bfc2 = tf.get_variable('b',[32], trainable = True, initializer = tf.contrib.layers.xavier_initializer())
+			fc2 = tf.nn.relu(tf.matmul(fc1, Wfc2) + bfc2, name = 'fc2')
+			self._parameters['fc2_W'] = Wfc2
+			self._parameters['fc2_b'] = bfc2
+			tf.add_to_collection('Reg', tf.reduce_sum(tf.square(Wfc2)))
+
+		with tf.variable_scope('nn/fc3') as scope:
+			Wfc3 = tf.get_variable('W',[32,1], trainable = True, initializer = tf.contrib.layers.xavier_initializer())
+			bfc3 = tf.get_variable('b',[1], trainable = True, initializer = tf.contrib.layers.xavier_initializer())
+			self._score = tf.matmul(fc2, Wfc3) + bfc3
+			self._parameters['fc3_W'] = Wfc3
+			self._parameters['fc3_b'] = bfc3
+			tf.add_to_collection('Reg', tf.reduce_sum(tf.square(Wfc3)))
+
+		self._pred = tf.squeeze(tf.sigmoid(self._score))
+		self._loss = tf.reduce_mean(tf.square(self._pred - self._output_placeholder))
+		for reg in tf.get_collection("Reg"):
+			self._loss += 0.5 * self._config.l2 * reg
+
+		self._global_step = tf.Variable(0, trainable = False)
+		lr = tf.train.exponential_decay(self._config.lr, decay_rate = self._config.decay_rate, global_step = self._global_step, decay_steps = self._config.decay_steps)
+
+		optimizer = tf.train.AdamOptimizer(lr)
+		self._train_op = optimizer.minimize(self._loss)
+
+	def run_epoch(self, sess, X, y, shuffle = True, batch_per_print = 2):
+		start = time.time()
+		num_batches = X.shape[0] // self._config.batch_size
+		idx = np.arange(X.shape[0])
+		if shuffle:
+			np.random.shuffle(idx)
+
+		len_eq = 20
+		batch_per_eq = (num_batches+len_eq-1)//len_eq
+
+		total_loss = []
+
+		for i in range(num_batches):
+			batch_idx = idx[i*self._config.batch_size:(i+1)*self._config.batch_size]
+			X_batch = X[batch_idx]
+			y_batch = y[batch_idx]
+			feed_dict = {self._input_placeholder:X_batch, self._output_placeholder:y_batch}
+			loss, _ = sess.run([self._loss, self._train_op], feed_dict)
+			total_loss.append(loss)
+			if (i+1)//batch_per_print*batch_per_print == i+1:
+				num_eq = (i+1)//batch_per_eq
+				sys.stdout.write('\r '+str(i+1)+' / '+str(num_batches)+' [' + '='*num_eq + ' '*(len_eq - num_eq) + '] - %0.2fs - loss: %0.4f  '%(float(time.time()-start),float(np.mean(total_loss))))
+				sys.stdout.flush()
+		batch_idx = idx[num_batches*self._config.batch_size:]
+		X_batch = X[batch_idx]
+		y_batch = y[batch_idx]
+		feed_dict = {self._input_placeholder:X_batch, self._output_placeholder:y_batch}
+		loss, _ = sess.run([self._loss, self._train_op], feed_dict)
+		total_loss.append(loss)
+		sys.stdout.write('\r '+str(num_batches)+' / '+str(num_batches)+' [' + '='*len_eq + '] - %0.2fs - loss: %0.4f  \n'%(float(time.time()-start),float(np.mean(total_loss))))
+		sys.stdout.flush()
+
+	def train(self, sess, X_train, y_train, X_val, y_val, X_test, y_test, version = 'v'):
+		val_error_current_best = float('Inf')
+		for i in range(self._config.num_epoch):
+			print('Epoch %d / %d'%(i+1,self._config.num_epoch))
+			self.run_epoch(sess, X_train, y_train)
+			train_error = self.error(sess, X_train, y_train)
+			val_error = self.error(sess, X_val, y_val)
+			test_error = self.error(sess, X_test, y_test)
+			print('train error: %0.4f; val error: %0.4f; test error: %0.4f \n' % (train_error, val_error, test_error))
+			if val_error < val_error_current_best:
+				val_error_current_best = val_error
+				self.save_parameters(sess, '../model/similarity/',version)
+
+	def error(self, sess, X, y):
+		num_batches = X.shape[0] // self._config.batch_size
+		e = 0.0
+		for i in range(num_batches):
+			feed_dict = {self._input_placeholder: X[i*self._config.batch_size:(i+1)*self._config.batch_size]}
+			pred = sess.run(self._pred, feed_dict)
+			similarity = y[i*self._config.batch_size:(i+1)*self._config.batch_size]
+			e += np.sum((pred - similarity) ** 2)
+		feed_dict = {self._input_placeholder: X[num_batches*self._config.batch_size:]}
+		pred = sess.run(self._pred, feed_dict)
+		similarity = y[num_batches*self._config.batch_size:]
+		e += np.sum((pred - similarity) ** 2)
+		return np.sqrt(e / X.shape[0])
---
+++ src/train_similarity.py
@@ -0,0 +1,105 @@
+from model_similarity import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--lr', help = 'learning rate')
+parser.add_argument('--dr', help = 'decay_rate')
+parser.add_argument('--ds', help = 'decay steps')
+parser.add_argument('--l2', help = 'regularization')
+parser.add_argument('--bs', help = 'batch size')
+parser.add_argument('--ne', help = 'number of epoches')
+parser.add_argument('--d', help = 'droupout')
+parser.add_argument('--v', help = 'version')
+args = parser.parse_args()
+
+version = args.v
+
+def parse_argument_NN(args):
+	para = {}
+	para['lr'] = float(args.lr) if args.lr != None else 0.001
+	para['decay_rate'] = float(args.dr) if args.dr != None else 0.9
+	para['decay_steps'] = int(args.ds) if args.ds != None else 700
+	para['l2'] = float(args.l2) if args.l2 != None else 0.0005
+	para['batch_size'] = int(args.bs) if args.bs != None else 128
+	para['num_epoch'] = int(args.ne) if args.ne != None else 20
+	para['dropout'] = float(args.d) if args.d != None else 0.5
+	return para
+
+def prepare_training_data(path_feature, path_similarity = '../data/similarity/'):
+	categories = []
+	files = []
+	labels = []
+	with open('../data/images.txt','r') as f:
+		n = int(f.readline().rstrip('\n'))
+		for _ in range(n):
+			category = f.readline().rstrip('\n')
+			categories.append(category)
+		while True:
+			temp = f.readline().rstrip('\n')
+			if temp == '':
+				break
+			temp_split = temp.split('\t')
+			files.append(temp_split[0])
+			labels.append(int(temp_split[1]))
+	labels = np.array(labels)
+
+	features = np.load(path_feature)['feature']
+
+	scores = dict()
+	score_ignore = dict()
+	score_ignore['Cell Phones & Accessories'] = 2
+	score_ignore['Watches'] = 474
+	score_ignore['Home Improvement'] = 495
+	for cate in categories:
+		scores[cate] = np.load(path_similarity + 'similarity_'+ cate +'.npz')['similarity']
+
+	x = []
+	y = []
+	skip = 0
+	for i in range(len(categories)):
+		num_skip = 500 * i - skip
+		category = categories[i]
+		for k1 in range(499):
+			if category in score_ignore:
+				if k1 == score_ignore[category]:
+					continue
+				elif k1 < score_ignore[category]:
+					feature_k1 = features[num_skip + k1]
+				else:
+					feature_k1 = features[num_skip + k1 - 1]
+			else:
+				feature_k1 = features[num_skip + k1]
+			for k2 in range(k1+1,500):
+				if category in score_ignore:
+					if k2 == score_ignore[category]:
+						continue
+					elif k2 < score_ignore[category]:
+						feature_k2 = features[num_skip + k2]
+					else:
+						feature_k2 = features[num_skip + k2 - 1]
+				else:
+					feature_k2 = features[num_skip + k2]
+				x.append(np.concatenate((feature_k1, feature_k2)))
+				y.append(scores[category][k1,k2])
+		if category in score_ignore:
+			skip += 1
+	return (np.array(x),np.array(y))
+
+path_feature = '../model/vgg/feature_3.npz'
+
+x, y = prepare_training_data(path_feature)
+
+# N_train = N // 10 * 7
+# N_val = N // 10 * 9
+# X_train = x[:N_train]
+# y_train = y[:N_train]
+# X_val = x[N_train:N_val]
+# y_val = y[N_train:N_val]
+# X_test = x[N_val:]
+# y_test = y[N_val:]
+
+# para = parse_argument_NN(args)
+# config = Config_NN(**para)
+# model_sim = model_nn(config)
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
+# model_sim.train(sess, X_train, y_train, X_val, y_val, X_test, y_test, version = 'v')

8a54b68870252ccae5403f93bd787aeb636b66ef
LouisChen <LouisChen1992@hotmail.com>
2017-06-10 23:41:29 -0700

    model similarity

--- src/train_similarity.py
+++ src/train_similarity.py
@@ -88,10 +88,9 @@ def prepare_training_data(path_feature, path_similarity = '../data/similarity/',
 				y_temp.append(scores[category][k1,k2])
 		x_temp = np.array(x_temp)
 		y_temp = np.array(y_temp)
-		# y_temp_sorted = sorted([(y_temp[k],k) for k in range(len(y_temp))], key = lambda t:t[0])
+		y_temp_sorted = sorted([(y_temp[k],k) for k in range(len(y_temp))], key = lambda t:t[0])
 		ind = np.arange(len(y_temp))
-		idx = ind[y_temp > 0.5]
-		# idx = np.array([k for _, k in y_temp_sorted[:500]] + [k for _, k in y_temp_sorted[-100:]])
+		idx = np.concatenate(ind[y_temp > 0.5], np.array([k for _, k in y_temp_sorted[-50:]]))
 		x.append(x_temp[idx])
 		y.append(y_temp[idx])
 		if category in score_ignore:
